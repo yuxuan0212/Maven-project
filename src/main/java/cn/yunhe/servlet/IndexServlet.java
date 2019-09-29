9244f37bc993740723f662c1bdadbc575fa94b89
杨 <1738463460@qq.com>
2019-09-29 14:43:04 +0800

    第一次提提交

---
+++ src/main/java/cn/yunhe/servlet/IndexServlet.java
@@ -0,0 +1,27 @@
+package cn.yunhe.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends HttpServlet {
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.setCharacterEncoding("gbk");
+		resp.getWriter().print("my first maven");
+		resp.getWriter().print("maven到底难不难？");
+	}
+	
+
+}
